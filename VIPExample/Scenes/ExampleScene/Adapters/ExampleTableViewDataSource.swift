48ed5311332b6e3cc06f3635b9937398ddcbbde9
Eduardo Bocato <dubocato@gmail.com>
2022-11-01 21:24:14 +0100

    Adding marks

--- VIPExample/Scenes/ExampleScene/Adapters/ExampleTableViewDataSource.swift
+++ VIPExample/Scenes/ExampleScene/Adapters/ExampleTableViewDataSource.swift
@@ -1,12 +1,16 @@
 import UIKit
 
 final class ExampleTableViewDataSource: NSObject, UITableViewDataSource {
+    // MARK: - Properties
+    
     typealias Item = ExampleScene.List.ViewModel.Item
     var items: [Item] = []
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
+    
+    // MARK: - UITableViewDataSource
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeue(
