888e6cd5babf3fdfe29ee723c2909cb50a19d5c5
Eduardo Bocato <dubocato@gmail.com>
2022-11-01 20:33:54 +0100

    Iteration 2

---
+++ VIPExample/Scenes/ExampleScene/ExampleAssembler.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+protocol ExampleAssembling {
+    func makeViewController() -> UIViewController
+}
+
+struct ExampleAssembler: ExampleAssembling {
+    func makeViewController() -> UIViewController {
+        let presenter: ExamplePresenter = .init()
+        
+        let itemsService: ItemsService = .init()
+        let getExampleItemsWorker: GetExampleItemsWorker = .init(
+            itemsService: itemsService
+        )
+        
+        let interactor: ExampleInteractor = .init(
+            presenter: presenter,
+            getExampleItemsWorker: getExampleItemsWorker
+        )
+        
+        let viewController: ExampleViewController = .init(
+            interactor: interactor
+        )
+        
+        let router: ExampleRouter = .init(
+            viewController: viewController
+        )
+        
+        router.dataStore = interactor
+        presenter.viewController = viewController
+        viewController.router = router
+        
+        return UINavigationController(
+            rootViewController: viewController
+        )
+    }
+}
